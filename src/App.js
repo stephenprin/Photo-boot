c6a157e71a1d1a3a70bf76b42c3090a8dcdeb630
stephenprin <stephenprince427@gmail.com>
2023-01-04 01:52:06 +0100

    displayed thhe image

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import Photo from "./Photo";
 import { FaSearch } from "react-icons/fa";
 
-const clientAccessKey =process.env.REACT_APP_ACCESS_KEY;
+const clientAccessKey = process.env.REACT_APP_ACCESS_KEY;
 
 const mainUrl = `https://api.unsplash.com/photos/`;
 const searchUrl = `https://api.unsplash.com/search/photos/`;
@@ -12,19 +12,38 @@ const searchUrl = `https://api.unsplash.com/search/photos/`;
 function App() {
   const [loading, setLoading] = React.useState(false);
   const [photos, setPhotos] = React.useState([]);
+  const [page, setPage] = React.useState(1);
+  const [query, setQuery] = React.useState([])
+  
 
-
+  console.log(photos);
 
   const fetchImages = async () => {
     let url;
-    url = `${mainUrl}?client_id=${clientAccessKey}`;
+    const urlPage = `&page=${page}`;
+    const urlQuery = `&query=${query}`;
+
+    if (query) { 
+      url = `${searchUrl}?client_id=${clientAccessKey}${urlPage}?${urlQuery}`;
+    } else {
+      url = `${mainUrl}?client_id=${clientAccessKey}${urlPage}`;
+    }
     try {
       setLoading(true);
       const response = await fetch(url);
       const data = await response.json();
-      setPhotos(data);
+      console.log(data);
+      setPhotos((oldPhotos) => {
+        if (query && page === 1) { 
+          return data.results;
+        }
+        else if (query) {
+          return [...oldPhotos, ...data.results];
+        } else {
+          return [...oldPhotos, ...data];
+         }
+      });
       setLoading(false);
-      
     } catch (error) {
       setLoading(false);
       console.log(error);
@@ -33,27 +52,46 @@ function App() {
 
   useEffect(() => {
     fetchImages();
+  }, [page]);
+
+  useEffect(() => {
+    const event = window.addEventListener("scroll", () => {
+      if (
+        !loading &&
+        window.innerHeight + window.scrollY >= document.body.scrollHeight - 2
+      ) {
+        setPage((oldPage) => {
+          return oldPage + 1;
+        });
+      }
+    });
+
+    return () => {
+      window.removeEventListener("scroll", event);
+    };
   }, []);
 
-
-  const handleSubmit = (e) => { 
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('hello');
-  }
+    fetchImages()
+    setPage(1)
+    
+  };
   return (
     <main>
       <section className="search">
         <form className="search-form">
-          <input type="text" placeholder="search" className="form-input" />
-          <button type="submit" className="submit-btn" onClick={handleSubmit}><FaSearch/></button>
+          <input type="text" placeholder="search" className="form-input" value={query} onChange={(e)=>setQuery(e.target.value)} />
+          <button type="submit" className="submit-btn" onClick={handleSubmit}>
+            <FaSearch />
+          </button>
         </form>
+        <button className="btn">Toggle</button>
       </section>
       <section className="photos">
         <div className="photos-center">
-          {photos.map((image, index) => { 
-            const { id } = image;
-            console.log(image);
-              return <Photo key={id} {...image} />
+          {photos.map((image, index) => {
+            return <Photo key={index} {...image} />;
           })}
         </div>
         {loading && <h2 className="loading">Loading...</h2>}
